4f0575de746fa969bb778f89556608db7913454b
Kiran Hart <kiran.hart@Live.com>
2018-11-22 14:49:52 -0500

    Finished UI design

--- src/Core.java
+++ src/Core.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseEvent;
@@ -17,10 +18,21 @@ public class Core {
 	private Shell shell;
 	private GameBoard gameBoard;
 	private int clickedButtonID = 0;
-	
-	//Data swt widgets 
-	private Button reset;
+	private int playerOneScore;
+	private int playerTwoScore;
+	private long startTime;
 
+	// Data swt widgets
+	private Button reset;
+	private Button playerOneScoreButton;
+	private Button playerTwoScoreButton;
+	private Button timeToMove;
+	
+	// constants
+	private final String RESET_BTN_TEXT = "Restart Game";
+	private final String TIME_TO_MOVE = "Move took: ";
+	private final String TIME_TO_MOVE_NO_MOVE = "No Move Yet";
+	
 	/*
 	 * The List to store the all of the buttons that can be clicked in the game.
 	 */
@@ -52,6 +64,11 @@ public class Core {
 
 		// Set the title
 		shell.setText("3D Tic Tac Toe");
+
+		// Set scores
+		playerOneScore = 0;
+		playerTwoScore = 0;
+		startTime = System.currentTimeMillis();
 
 		// Setup the grids / buttons
 		generateGridButtons();
@@ -90,7 +107,7 @@ public class Core {
 			button.setSize(new Point(60, 60));
 			button.setLocation(new Point(x, y));
 			// button.setFont(new Font(display, "Tahoma", 10, SWT.BOLD));
-			//button.setText("" + i);
+			// button.setText("" + i);
 			button.setVisible(true);
 
 			// Add 60 to x
@@ -107,7 +124,7 @@ public class Core {
 				 */
 
 				if (y == 180 || y == 380) {
-					//Button filler = new Button(shell, SWT.BORDER);
+					// Button filler = new Button(shell, SWT.BORDER);
 					Label filler = new Label(shell, SWT.PUSH);
 					filler.setSize(new Point(180, 20));
 					filler.setLocation(new Point(x, y));
@@ -120,12 +137,46 @@ public class Core {
 			// Finally add the buttons to the list for later use.
 			gridButtons.add(button);
 		}
-		
+
+		// Reset button
 		reset = new Button(shell, SWT.PUSH);
 		reset.setSize(new Point(180, 60));
 		reset.setLocation(new Point(195, 510));
-		reset.setText("Restart Game");
+		reset.setText(RESET_BTN_TEXT);
 		reset.setVisible(true);
+
+		// Player Score Buttons //
+		playerOneScoreButton = new Button(shell, SWT.PUSH);
+		playerOneScoreButton.setSize(new Point(180, 60));
+		playerOneScoreButton.setLocation(new Point(195, 25));
+		playerOneScoreButton.setText("Player One Wins: " + playerOneScore);
+		playerOneScoreButton.setVisible(true);
+
+		playerTwoScoreButton = new Button(shell, SWT.PUSH);
+		playerTwoScoreButton.setSize(new Point(180, 60));
+		playerTwoScoreButton.setLocation(new Point(195, 95));
+		playerTwoScoreButton.setText("Player Two Wins: " + playerTwoScore);
+		playerTwoScoreButton.setVisible(true);
+		
+		timeToMove = new Button(shell, SWT.PUSH);
+		timeToMove.setSize(new Point(180, 60));
+		timeToMove.setLocation(new Point(195, 440));
+		timeToMove.setText(TIME_TO_MOVE_NO_MOVE);
+		timeToMove.setVisible(true);
+		
+		Label label = new Label(shell, SWT.BORDER);
+		label.setSize(new Point(180, 255));
+		label.setLocation(new Point(195, 170));
+		label.setText("===== Game Designers ===== \n\n" +
+					  "	    Kiran Hart \n\n" + 
+					  "	    Rahul Tailor \n\n" + 
+					  "	    Antonio N-J \n\n" + 
+					  "	    Daniel Gopal \n\n" +
+					  "	    Calvin Ye \n\n" + 
+					  "	    Robin Saran \n\n" +
+					  "---------------------------------------\n" +
+					  "           Thank you for playing");
+		label.setVisible(true);
 	}
 
 	/*
@@ -137,6 +188,29 @@ public class Core {
 		for (int i = 0; i < gridButtons.size(); i++) {
 			gridButtons.get(i).addMouseListener(new MouseListeners(gridButtons.get(i)));
 		}
+	}
+	
+	/*
+	 * Restart the game
+	 */
+	private void resetGame() {
+		//Reset GameBoard
+		
+		//Reset Text;
+		for (Button b : gridButtons) {
+			b.setText("");
+		}
+		
+		timeToMove.setText(TIME_TO_MOVE_NO_MOVE);
+		updateButtonScores();
+	}
+	
+	/*
+	 * Update button score;
+	 */
+	private void updateButtonScores() {
+		playerOneScoreButton.setText("Player One Wins: " + playerOneScore);
+		playerTwoScoreButton.setText("Player Two Wins: " + playerTwoScore);
 	}
 	
 	/*
@@ -160,40 +234,54 @@ public class Core {
 		@Override
 		public void mouseDown(MouseEvent e) {
 
-			//Get the button index in the array on click.
+			// Get the button index in the array on click.
 			clickedButtonID = gridButtons.indexOf(button);
-			
+
 			/*
-			 * Mr. Nestor's mapping method to get the specific level, row, column 
-			 * depending on which button is clicked and it's ID, in a single
-			 * dimensional array. 
+			 * Mr. Nestor's mapping method to get the specific level, row,
+			 * column depending on which button is clicked and it's ID, in a
+			 * single dimensional array.
 			 * https://github.com/Greg5519/ICS4C0/blob/master/Topic%20B%20Programming%20Skills/3D%20TTT%20Mapper.txt
 			 */
 			double remainder = clickedButtonID % 9;
 			int level = (int) clickedButtonID / 9;
 			int row = (int) remainder / 3;
 			int col = (int) remainder % 3;
-			
-			//Check if the move can be made to the specific level, row and column by checking if the space is blank 
+
+			// Check if the move can be made to the specific level, row and
+			// column by checking if the space is blank
 			if (gameBoard.checkMove(Integer.valueOf(level + 1), Integer.valueOf(row + 1), Integer.valueOf(col + 1)).equalsIgnoreCase(GameBoard.playerFree)) {
-				//Make a move depending on who's turn it is on the specific level, row and column.
-				if (gameBoard.makeMove((playerXTurn) ? GameBoard.playerX :  GameBoard.playerO, Integer.valueOf(level + 1), Integer.valueOf(row + 1), Integer.valueOf(col + 1))) {
-				
+				// Make a move depending on who's turn it is on the specific
+				// level, row and column.
+				if (gameBoard.makeMove((playerXTurn) ? GameBoard.playerX : GameBoard.playerO, Integer.valueOf(level + 1), Integer.valueOf(row + 1), Integer.valueOf(col + 1))) {
+
 					System.out.println("Made move to level: " + level + " row: " + row + " col: " + col);
-					
+
 					/*
-					 * Update the button text to let the player know which player
-					 * made a move onto that grid slot.
+					 * Update the button text to let the player know which
+					 * player made a move onto that grid slot.
 					 */
-					button.setText((playerXTurn) ? GameBoard.playerX :  GameBoard.playerO);
+					button.setText((playerXTurn) ? GameBoard.playerX : GameBoard.playerO);
 					
-					//Swap the player turn.
-					playerXTurn = !playerXTurn;
+					if (isWin(GameBoard.playerX)) {
+						resetGame();
+						playerOneScore++;
+					} else if (isWin(GameBoard.playerO)) {
+						resetGame();
+						playerTwoScore++;
+					} else {
+						// Swap the player turn.
+						playerXTurn = !playerXTurn;
+						
+						// Record Time it took to make move
+						long secs = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - startTime);
+						timeToMove.setText(String.valueOf((secs <= 0) ? "Move took less than 1 sec." : TIME_TO_MOVE + String.valueOf(secs) + "s"));
+						startTime = System.currentTimeMillis();
+					}
 				}
 			} else {
 				System.out.println("The player: \"" + button.getText() + "\" already chose there.");
 			}
-
 		}
 
 		@Override
@@ -204,9 +292,12 @@ public class Core {
 		public void mouseUp(MouseEvent e) {
 		}
 
-
 	}
-
+	
+	private boolean isWin(String x) {
+		return false;
+	}
+	
 	/*
 	 * This is the main method, the program will call this method and run any
 	 * code within this class upon pressing start
